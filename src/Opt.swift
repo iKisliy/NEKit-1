6f60e6a8b9fda0886893f1b8768ceb77cfb3973e
hopwesley <ws@sinoc.io>
2020-09-17 12:17:32 +0800

    re

--- src/Opt.swift
+++ src/Opt.swift
@@ -1,8 +1,12 @@
 import Foundation
 
 public struct Opt {
+<<<<<<< HEAD
     
     public static var MAXNWTCPSocketReadDataSize = 1 << 16 //128 * 1024
+=======
+    public static var MAXNWTCPSocketReadDataSize = 1<<20 //128 * 1024
+>>>>>>> make a big buffer
 
     // This is only used in finding the end of HTTP header (as of now). There is no limit on the length of http header, but Apache set it to 8KB
     public static var MAXNWTCPScanLength = 8912
